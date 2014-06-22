8071451d23c04628c1c5de24190d2ae2cc28dfc2
satish28 <satish28888@gmail.com>
2014-06-22 23:27:09 +0530

    Update models.py

--- blogg/login/models.py
+++ blogg/login/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractBaseUser
 class MyUser(AbstractBaseUser):
 	email = models.EmailField(max_length=254, unique=True, db_index=True, primary_key=True)
 	USERNAME_FIELD = 'email'
-	firstname = models.CharField(max_length=50,null = False,blank = True)
-	lastname = models.CharField(max_length=50,null = False,blank = True)
+	firstname = models.CharField(max_length=50, null=False, blank=False)
+	lastname = models.CharField(max_length=50, null=False, blank=True)
 
 # Create your models here.
