1e724cd97ba34b2b882e52190ec3c0333afd6fc0
satish28 <satish28888@gmail.com>
2014-06-23 00:52:01 +0530

    Update models.py

--- blogg/login/models.py
+++ blogg/login/models.py
@@ -5,6 +5,7 @@ class MyUser(AbstractBaseUser):
 	email = models.EmailField(max_length=254, unique=True, db_index=True, primary_key=True)
 	firstname = models.CharField(max_length=50, null=False, blank=False)
 	lastname = models.CharField(max_length=50, null=False, blank=True)
+	
 	USERNAME_FIELD = 'email'
 
 # Create your models here.
