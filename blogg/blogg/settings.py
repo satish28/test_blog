d859bce0a0c7247a2b88afb593adf1df6d55a631
satish28 <satish2888@gmail.com>
2014-07-07 09:03:24 +0530

    modified db settings for working on local host

--- blogg/blogg/settings.py
+++ blogg/blogg/settings.py
@@ -69,9 +69,9 @@ WSGI_APPLICATION = 'blogg.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', 
-        'NAME': '', 
-        'USER': '',
-        'PASSWORD': '',
+        'NAME': 'mydb', 
+        'USER': 'satish',
+        'PASSWORD': 'satish28',
         'HOST': 'localhost', 
         'PORT': '',          
     }
